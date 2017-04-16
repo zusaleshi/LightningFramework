71b3d8d22481f93e17694c9453b81f7c326594cc
Neosteam <kakashi@live.cn>
2017-04-16 16:51:34 +0800

    pp

--- Vendor/LightningFramework/Config.Global/config.php
+++
@@ -1,7 +0,0 @@
-<?php if(! defined('framework_name')) exit('No direct script access allowed');
-$config_global = array();
-
-
-
-
-return $config_global;
--- Vendor/LightningFramework/Exception/LFException.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Lightning\Exception;
-if(! defined('framework_name')) exit('No direct script access allowed');
-
-use \Exception as Base;
-
-class LFException extends Base {}
